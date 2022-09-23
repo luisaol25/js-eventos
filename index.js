fc17f5f687572aa144a79bbadfeafa007a5defa8
Luisa Olivares <luisaol25@gmail.com>
2022-09-22 22:05:57 -0300

    entrega 2 con json storage eventos y dom

--- index.js
+++ index.js
@@ -1,62 +1,46 @@
 window.onload = mostrarTablaHistorica()
 
+
 function mostrarEnFahrenheit() {
   let valorCelsius = parseInt(document.getElementById("cajaCelsius").value);
   let valorConvertido = valorCelsius * 1.8 + 32;
   document.getElementById("cajaFah").value = valorConvertido;
 
-  const map1 = new Map();
+  let objeto = {}
+  objeto.valorCelsius = valorCelsius.toFixed(1)
+  objeto.valorFah = valorConvertido.toFixed(1)
 
-  map1.set("gradosC", valorCelsius)
-  map1.set("gradosF", valorConvertido)
+  let valores = []
+  if (localStorage.getItem("valores")){
+    valores = JSON.parse(localStorage.getItem("valores"))
+  }
 
-  arr = []
-  arr.push(map1)
 
-  console.log(arr)
-  localStorage.setItem("valoresH", arr);
+  valores.push(objeto)
+  localStorage.setItem("valores", JSON.stringify(valores))
   
+  mostrarTablaHistorica()
 }
 
 function mostrarTablaHistorica() {
-  // creates a <table> element and a <tbody> element
-  const tbl = document.createElement("table");
-  const tblBody = document.createElement("tbody");
+    
+    
+    let div = document.getElementById("valoresHis")
+    let html = ""
+    if (localStorage.length > 0){
 
-  let arregloStored = localStorage.getItem("valoresH")
+        let elementos = JSON.parse(localStorage.getItem("valores"))
+        
+        for (let i = 0; i < elementos.length; i++){
 
-  for (let indice = 0; indice < arregloStored.length; indice++){ 
+            objetoElemento = elementos[i]
+            html += "Grados Celsius: " + objetoElemento.valorCelsius + " | Grados Fahrenheit: " + objetoElemento.valorFah +  "<br/>"
+        }
 
-    let mapa = arregloStored[indice]
-    console.log(mapa)
-
-
-  // creating all cells
-  for (let i = 0; i < 2; i++) {
-    // creates a table row
-    const row = document.createElement("tr");
-
-    for (let j = 0; j < 2; j++) {
-      // Create a <td> element and a text node, make the text
-      // node the contents of the <td>, and put the <td> at
-      // the end of the table row
-      const cell = document.createElement("td");
-      /*const cellText = document.createTextNode(`Grado en Celsius ${mapa.get("gradosC")}, Grados en Fahrenheit ${mapa.get("gradosF")}`);*/
-      const cellText = document.createTextNode(0);
-      cell.appendChild(cellText);
-      row.appendChild(cell);
-    }
-
-    // add the row to the end of the table body
-    tblBody.appendChild(row);
-  }
-
-}
-
-  // put the <tbody> in the <table>
-  tbl.appendChild(tblBody);
-  // appends <table> into <body>
-  document.body.appendChild(tbl);
-  // sets the border attribute of tbl to '2'
-  tbl.setAttribute("border", "2");
+        div.innerHTML = html
+    
+    }   
+    
+    
+        
 }
