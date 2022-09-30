dee3ff034d70b3856d50da202246eff0c8c8dcab
Luisa Olivares <luisaol25@gmail.com>
2022-09-29 22:07:59 -0300

    se agrega lib sweet alert

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
-window.onload = mostrarTablaHistorica()
-
+window.onload = mostrarTablaHistorica();
 
 function mostrarEnFahrenheit() {
   let valorCelsius = parseInt(document.getElementById("cajaCelsius").value);
   let valorConvertido = valorCelsius * 1.8 + 32;
   document.getElementById("cajaFah").value = valorConvertido;
 
-  let objeto = {}
-  objeto.valorCelsius = valorCelsius.toFixed(1)
-  objeto.valorFah = valorConvertido.toFixed(1)
+  Swal.fire("El Valor convertido en Fahrenheit es: " + valorConvertido);
 
-  let valores = []
-  if (localStorage.getItem("valores")){
-    valores = JSON.parse(localStorage.getItem("valores"))
+  let objeto = {};
+  objeto.valorCelsius = valorCelsius.toFixed(1);
+  objeto.valorFah = valorConvertido.toFixed(1);
+
+  let valores = [];
+  if (localStorage.getItem("valores")) {
+    valores = JSON.parse(localStorage.getItem("valores"));
   }
 
+  valores.push(objeto);
+  localStorage.setItem("valores", JSON.stringify(valores));
 
-  valores.push(objeto)
-  localStorage.setItem("valores", JSON.stringify(valores))
-  
-  mostrarTablaHistorica()
+  mostrarTablaHistorica();
 }
 
 function mostrarTablaHistorica() {
-    
-    
-    let div = document.getElementById("valoresHis")
-    let html = ""
-    if (localStorage.length > 0){
+  let div = document.getElementById("valoresHis");
+  let html = "";
+  if (localStorage.length > 0) {
+    let elementos = JSON.parse(localStorage.getItem("valores"));
 
-        let elementos = JSON.parse(localStorage.getItem("valores"))
-        
-        for (let i = 0; i < elementos.length; i++){
+    for (let i = 0; i < elementos.length; i++) {
+      objetoElemento = elementos[i];
+      html +=
+        "Grados Celsius: " +
+        objetoElemento.valorCelsius +
+        " | Grados Fahrenheit: " +
+        objetoElemento.valorFah +
+        "<br/>";
+    }
 
-            objetoElemento = elementos[i]
-            html += "Grados Celsius: " + objetoElemento.valorCelsius + " | Grados Fahrenheit: " + objetoElemento.valorFah +  "<br/>"
-        }
-
-        div.innerHTML = html
-    
-    }   
-    
-    
-        
+    div.innerHTML = html;
+  }
 }
